6d0976114742f4bc54ddeab0adf5395e6066e9ee
Ajaya Krishna Viyyapu <ajay@test.com>
2020-06-10 14:09:22 +0200

    # 1 Working Simulation Commit

--- GameOfLife/src/main/java/com/ajaya/Simulation.java
+++ GameOfLife/src/main/java/com/ajaya/Simulation.java
@@ -14,6 +14,72 @@ public class Simulation {
 		this.board = new int[width][height];
 	}
 	
+	
+	//Simulation from one board to another board
+	public void step() {
+		
+		// Editing the current board inline while checking it. 
+		// This will create problems as we are changing the original state of the board. 
+		// So when the next line is processed it is calculated according to the changed board not the original board
+		// So creating a new board array and applying the changes on it based on the original board
+		// Finally assign the new board to the original board
+		
+		int[][] newBoard = new int[width][height];
+		
+		for (int y = 0; y < height; y++) {
+				for (int x = 0; x < width; x++) {
+					
+						int aliveNeighbours = countAliveNeighbours(x,y);
+						
+						if(stepState(x,y) == 1) { // Live Cell Conditions
+							if(aliveNeighbours < 2) //Under Population Condition
+								newBoard[x][y] = 0;
+							else if(aliveNeighbours == 2 || aliveNeighbours == 3) //Keeping Alive Condition
+								newBoard[x][y] = 1;
+							else if(aliveNeighbours > 3) //Over Population Condition
+								newBoard[x][y] = 0;
+						}
+						else {
+							if(aliveNeighbours == 3) //Dead Cell Condition - More than three alive neighbours - Activate the Dead Cell
+								newBoard[x][y] = 1;
+						}
+					
+					}
+						
+				}
+		
+		this.board = newBoard;
+	}
+	
+	public int countAliveNeighbours(int x, int y) {
+		int count=0;
+		
+		count += stepState(x-1,y-1);
+		count += stepState(x,y-1);
+		count += stepState(x+1,y-1);
+		
+		count += stepState(x-1,y);
+		count += stepState(x+1,y);
+		
+		count += stepState(x-1,y+1);
+		count += stepState(x,y+1);
+		count += stepState(x+1,y+1);
+		
+		return count;
+	}
+	
+	//Preventing the countAliveNeighbours function from going away from the board boundaries
+	public int stepState(int x, int y) {
+	
+		if(x < 0 || x >= width)
+			return 0;
+		
+		if(y < 0 || y >= height)
+			return 0;
+			
+		return this.board[x][y];
+	}
+	
 	public void printBoard() {
 		
 		System.out.println("---");
@@ -60,6 +126,13 @@ public class Simulation {
 		
 		s.printBoard();
 		
+		s.step();
+		
+		s.printBoard();
+		
+		s.step();
+		
+		s.printBoard();
 	}
 	
 }
